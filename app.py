a36531ee0814bf59fcfd1afd6af4ae96550b4d81
Zetsuboushii <zetsuboushii@gmail.com>
2025-02-21 00:23:56 +0100

    feat: characters list, home

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 import random
-
+from datetime import date
 from flask import Flask, render_template, abort, g
 import json
 
 app = Flask(__name__)
 
 
-def load_characters_from_json():
-    with open('static/json/characters.json') as file:
-        return json.load(file)
-
-
-characters = load_characters_from_json()
-
-
 @app.before_request
 def before_request():
     g.site_title = "Tome of the Vastlands"
-    g.version_number = "b3.⛩"
+    g.version_number = "v3.⛩"
 
     g.ingame_date = "24.12.394"
     g.lore_days = ["Lunesdag", "Flamdag", "Quellsdag", "Waldsdag", "Goldag", "Terrasdag", "Sunnesdag"]
@@ -26,22 +18,56 @@ def before_request():
 
     g.static = "/static/"
 
+    g.categories = {
+        "Charaktere": "characters",
+        "Kompendien": "compendium"
+    }
+
     apex_domain = "zetsuboushii.site/"
-    g.img_host = "https://images." + apex_domain + "/dnd/characters/"
+    image_subdomain = "https://images."
+    g.img_host = image_subdomain + apex_domain + "/dnd/"
+    g.img_host_resized = image_subdomain + apex_domain + "/resized/dnd/"
 
     g.random_banner = random.randint(1, 9)
 
 
+def load_from_json(filename):
+    with open(f'static/json/{filename}.json') as file:
+        return json.load(file)
+
+
+characters_list = load_from_json("characters")
+news_list = load_from_json("news")
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    birthday_characters = []
+    for char in characters_list:
+        data = char.copy()
+
+        if data.get('birthday') != "":
+            birthday_day, birthday_month, _ = map(int, data['birthday'].split('.'))
+
+            current_day = date.today()
+            print(current_day.day, current_day.month)
+            if f"{birthday_day:02}" == f"{current_day.day:02}" and f"{birthday_month:02}" == f"{current_day.month:02}":
+                birthday_characters.append(data)
+
+    rand_char = random.choice(characters_list)
+
+    return render_template('index.html', birthday_characters=birthday_characters, news=news_list, rand_char=rand_char)
+
+
+@app.route('/characters/')
+def characters():
+    return render_template('characters.html', characters=characters_list)
 
 
 @app.route('/characters/<character_name>')
 def character(character_name):
     character_data = None
 
-    for char in characters:
+    for char in characters_list:
         if char['name'].lower() == character_name.lower():
             data = char.copy()
 
@@ -52,7 +78,8 @@ def character(character_name):
                 data['birth_day'] = str(birthday_day)
                 data['birth_month'] = g.lore_months[birthday_month - 1]
                 data['birth_year'] = str(birthday_year)
-                data['age'] = ingame_year - birthday_year - ((ingame_month, ingame_day) < (birthday_month, birthday_day))
+                data['age'] = ingame_year - birthday_year - (
+                        (ingame_month, ingame_day) < (birthday_month, birthday_day))
 
             match data.get('status'):
                 case "true":
@@ -71,6 +98,10 @@ def character(character_name):
         abort(404)
 
     return render_template('character.html', character=character_data)
+
+@app.route('/compendium/')
+def compendium():
+    return render_template('compendia.html')
 
 
 if __name__ == '__main__':
