731fde53ed3a7b3c1fee4961240c45a9011a95e3
Zetsuboushii <65507051+Zetsuboushii@users.noreply.github.com>
2025-02-24 21:57:12 +0000

    Deploying to gh-pages from @ Zetsuboushii/Vastlands-Wiki@0aa7afe390f8a1be0d521922d74b7ae817fac412 🚀

---
+++ static/json/clean_json.py
@@ -0,0 +1,24 @@
+import re
+
+def repair_and_remove_newlines(filepath):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        content = re.sub(r'},\s*}', '}]', content)
+        content = re.sub(r',\s*]', ']', content)
+
+        content = re.sub(r'"(text|excerpt)":\s*"([^"]*)"', lambda match: '"{}": "{}"'.format(match.group(1), re.sub(r'[\r\n]+', ' ', match.group(2))), content)
+
+        with open(filepath, 'w', encoding='utf-8') as file:
+            file.write(content)
+
+        print(f"File '{filepath}' is now good.")
+    except FileNotFoundError:
+        print(f"File '{filepath}' not found.")
+    except Exception as e:
+        print(f"Exception: {e}")
+
+if __name__ == "__main__":
+    filepath = "places.json"
+    repair_and_remove_newlines(filepath)
