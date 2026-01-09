380f5cfe2f34d5889ffbc99a34b00005d3eb9690
LukeGrasser <luke.grasser@deutschebahn.com>
2026-01-09 18:42:55 +0100

    content update: - altorn - verdorn

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def load_from_json(filename):
 @app.before_request
 def before_request():
     g.site_title = "Tome of the Vastlands"
-    g.version_number = "5.5.0"
+    g.version_number = "5.5.1"
 
     g.ingame_date = load_from_json("current_date")["current_ingame_date"]
     g.lore_days = ["Lunesdag", "Flamdag", "Quellsdag", "Waldsdag", "Goldag", "Terrasdag", "Sunnesdag"]
