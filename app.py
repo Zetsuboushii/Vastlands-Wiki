ab85d64d32fe957b6aec840ffb3c163ec3b387a7
Zetsuboushii <zetsuboushii@gmail.com>
2026-02-06 22:21:53 +0100

    change: rename locations.json to geographium.json

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def load_from_json(filename):
 @app.before_request
 def before_request():
     g.site_title = "Tome of the Vastlands"
-    g.version_number = "5.5.16"
+    g.version_number = "5.5.17"
 
     g.ingame_date = load_from_json("current_date")["current_ingame_date"]
     g.lore_days = ["Lunesdag", "Flamdag", "Quellsdag", "Waldsdag", "Goldag", "Terrasdag", "Sunnesdag"]
@@ -238,7 +238,7 @@ def theologarium_entry(entry_name):
 
 @app.route('/geographium/')
 def geographium():
-    places_tree = load_from_json("locations")
+    places_tree = load_from_json("compendia/geographium")
     return render_template('locations.html', locations=places_tree)
 
 
